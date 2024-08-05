358556e612a33dc9345e59166aa51b6e763da6b4
Renoks-Dev <renoks150@gmail.com>
2024-08-05 13:22:01 +0100

    Add serverinfo command

---
+++ commands/info/server-info.js
@@ -0,0 +1,58 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("serverinfo")
+  .setDescription("Display information about this server.");
+
+export async function execute(interaction) {
+  try {
+    await interaction.deferReply({ ephemeral: true });
+
+    const { guild } = interaction;
+    const owner = await guild.fetchOwner();
+
+    const embed = new EmbedBuilder()
+      .setColor("Blurple")
+      .setTitle(`${guild.name}`)
+      .setThumbnail(guild.iconURL({ dynamic: true }))
+      .addFields(
+        { name: "Server ID", value: guild.id.toString(), inline: true },
+        {
+          name: "Owner",
+          value: `<@${owner.id}>`,
+          inline: true,
+        },
+        { name: "Members", value: `${guild.memberCount}`, inline: true },
+        {
+          name: "Created At",
+          value: `${guild.createdAt.toDateString()}`,
+          inline: true,
+        },
+        {
+          name: "Verification Level",
+          value: `${guild.verificationLevel}`,
+          inline: true,
+        },
+        {
+          name: "Boost Level",
+          value: guild.premiumTier ? `Tier ${guild.premiumTier}` : "None",
+          inline: true,
+        },
+        {
+          name: "Boosts",
+          value: `${guild.premiumSubscriptionCount || 0}`,
+          inline: true,
+        }
+      )
+      .setImage(guild.bannerURL({ dynamic: true }) || null)
+      .setTimestamp()
+      .setFooter({
+        text: "Server Info",
+        iconURL: guild.iconURL({ dynamic: true }),
+      });
+
+    await interaction.editReply({ embeds: [embed], ephemeral: true });
+  } catch (error) {
+    console.error(error);
+  }
+}
