a19432de258c6af071b29ddee2b075fe7dbe3b4c
Oskar-Ciebien <renoks150@gmail.com>
2025-08-05 23:03:54 +0100

    Update interactionCreate.js after logger and lint changes

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,4 +1,5 @@
-import { Events } from "discord.js";
+import { Events } from 'discord.js';
+import logger from '../utils/logger.js';
 
 export const name = Events.InteractionCreate;
 export async function execute(interaction) {
@@ -7,24 +8,37 @@ export async function execute(interaction) {
   const command = interaction.client.commands.get(interaction.commandName);
 
   if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
+    logger.error(`No command matching ${interaction.commandName} was found.`);
     return;
   }
 
   try {
     await command.execute(interaction);
   } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+    logger.error('Command execution error:', {
+      command: interaction.commandName,
+      user: interaction.user.tag,
+      guild: interaction.guild?.name,
+      error: error.message,
+      stack: error.stack,
+    });
+
+    const errorMessage = 'There was an error while executing this command!';
+
+    try {
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: errorMessage,
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: errorMessage,
+          ephemeral: true,
+        });
+      }
+    } catch (replyError) {
+      logger.error('Failed to send error message to user:', replyError);
     }
   }
 }
