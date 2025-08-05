d3b69552c22ab4b7bbfd0471ad7bc6138acb4b07
Oskar-Ciebien <renoks150@gmail.com>
2025-08-05 23:00:52 +0100

    Update ready event with new dynamic activities

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,35 @@
-import { Events } from "discord.js";
+import { Events, ActivityType } from 'discord.js';
+import logger from '../utils/logger.js';
 
 export const name = Events.ClientReady;
 
 export const once = true;
 
 export function execute(client) {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
+  logger.info(`Ready! Logged in as ${client.user.tag}`);
+
+  // Set bot activity
+  client.user.setActivity({
+    name: `${client.commands.size} commands | /help`,
+    type: ActivityType.Watching,
+  });
+
+  // Log bot statistics
+  logger.info(`Bot is in ${client.guilds.cache.size} guilds`);
+  logger.info(`Serving ${client.users.cache.size} users`);
+
+  // Set up activity rotation (optional)
+  setInterval(() => {
+    const activities = [
+      { name: `${client.commands.size} commands`, type: ActivityType.Watching },
+      {
+        name: `${client.guilds.cache.size} servers`,
+        type: ActivityType.Watching,
+      },
+      { name: '/help for commands', type: ActivityType.Playing },
+    ];
+
+    const activity = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity(activity);
+  }, 10000); // Change every 30 seconds 30000
 }
